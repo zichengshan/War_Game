eee509426e79b45528ed6c2b950ab4ef5b699736
Zicheng.S <zichengshan@gmail.com>
2022-04-04 00:57:34 -0700

    keep score

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let deckId = 0
+let computerScore = 0
+let myScore = 0
 const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
 const headerEl = document.getElementById("header")
 const remainingText = document.getElementById("remaining")
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
 
 newDeckBtn.addEventListener("click", handleClick)
 drawCardBtn.addEventListener("click", drawCards)
@@ -23,9 +27,11 @@ function drawCards() {
         .then(res => res.json())
         .then(data => {
             remainingText.textContent = `Remaining Cards: ${data.remaining}`
-            cardsContainer.children[0].innerHTML = `<img class="card" src=${data.cards[0].image} />`
-            cardsContainer.children[1].innerHTML = `<img class="card" src=${data.cards[1].image} />`
+            cardsContainer.children[1].innerHTML = `<img class="card" src=${data.cards[0].image} />`
+            cardsContainer.children[3].innerHTML = `<img class="card" src=${data.cards[1].image} />`
             headerEl.innerText = determineCardWinner(data.cards[0], data.cards[1])
+            computerScoreEl.textContent = `Computer Score: ${computerScore}`
+            myScoreEl.textContent = `My Score: ${myScore}`
 
             if(data.remaining === 0){
                 drawCardBtn.disabled = true
@@ -38,8 +44,10 @@ function determineCardWinner(card1, card2) {
     const card1ValueIndex = valueOptions.indexOf(card1.value)
     const card2ValueIndex = valueOptions.indexOf(card2.value)
     if (card1ValueIndex > card2ValueIndex) {
+        computerScore++
         return "Computer wins!"
     } else if (card1ValueIndex < card2ValueIndex) {
+        myScore++
         return "You win!"
     } else {
         return "War"
